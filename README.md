ab86c2e42ab91d62b965462e03e614451a66b9ee
Arman Hosseini <namra.1377@gmail.com>
2019-05-17 23:44:14 +0430

    Update Core

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # JSON-Web-Editor
-JSON Web Editor Tools
+This is a small tool for your JSON objects and this tool is written by **PHP language**. you can manipulating your JSON data via a web editor.
+
+![Cover](assets/img/JSON-Web-Editor.png)
+
+## How to install
+
+Download the project and put it at the root of your project.
+
+## Usage
+
+By default, you must put your JSON files in the following path:
+`/report`
+
+You can change the default path using the `config.php` edit and put your desired path in the variable **$json_dir**.
+
+Enjoy and if you have an idea to improve this tool please contact me.
--- config.php
+++ config.php
@@ -4,15 +4,15 @@ $json_dir = "report";
 $json_dir .= DIRECTORY_SEPARATOR;
 
 require_once( "funcs.php" );
-$head   = '	<link rel="stylesheet" href="css/bootstrap.min.css">
-	<script src="js/jquery.min.js"></script>
-	<script src="js/popper.min.js"></script>
-	<script src="js/bootstrap.min.js"></script>
+$head   = '	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<script src="assets/js/jquery.min.js"></script>
+	<script src="assets/js/popper.min.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
 
-	<script src="js/jquery.bdt.js"></script>
-	<link href="css/jquery.bdt.css" rel="stylesheet">
-	<link href="css/font-awesome.min.css" rel="stylesheet">
-	<script src="js/jquery.sortelements.js"></script>'.PHP_EOL;
+	<script src="assets/js/jquery.bdt.js"></script>
+	<link href="assets/css/jquery.bdt.css" rel="stylesheet">
+	<link href="assets/css/font-awesome.min.css" rel="stylesheet">
+	<script src="assets/js/jquery.sortelements.js"></script>'.PHP_EOL;
 	
 $header = '<div class="pb-2 mt-4 mb-2 border-bottom">
 		  <h2>JSON editor</h2>
